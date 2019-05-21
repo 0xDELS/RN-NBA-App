a8c19441cac4d2220b0d58aad4dd3b84cabd51f0
Diogo Silva <diogo@diogo-silva.pt>
2019-05-21 14:39:54 +0100

    News Article

--- src/components/news/newsArticle.js
+++ src/components/news/newsArticle.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+
+import Moment from 'moment';
 
 class NewsArticleComponent extends Component {
+
+    formatText(content) {
+        const text = content.replace(/<p[^>]*>/g, '').replace(/<\/p>/g, '');
+        return text;
+    }
     render() {
+        const params = this.props.navigation.state.params;
         return (
-            <View style={styles.container}>
-                <Text>NewsArticleComponent</Text>
-            </View>
+            <ScrollView style={{backgroundColor: '#f0f0f0'}}>
+                <Image
+                    style={{height: 250}}
+                    source={{uri: params.image}}
+                    resizeMode='cover'
+                />
+
+                <View style={styles.articleContainer}>
+                    <View>
+                        <Text style={styles.articleTitle}>
+                            {params.title}
+                        </Text>
+                        <Text style={styles.articleData}>
+                            {params.team} - Posted at {Moment(params.date).format('d MMMM')}
+                        </Text>
+                    </View>
+
+                    <View style={styles.articleContent}>
+                        <Text style={styles.articleText}>
+                            {this.formatText(params.content)}
+                        </Text>
+                    </View>
+                </View>
+
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
+    articleContainer: {
+        padding: 10
+    },
+    articleTitle:{
+        fontSize: 23,
+        color: '#323232',
+        fontFamily: 'Roboto-Bold'
+    },
+    articleData:{
+        fontSize: 12,
+        color: '#828282',
+        fontFamily: 'Roboto-Light'
+    },
+    articleContent:{
+        marginTop: 30
+    },
+    articleText:{
+        fontSize: 13,
+        lineHeight: 20,
+        fontFamily: 'Roboto-Light'
     }
 });
 
