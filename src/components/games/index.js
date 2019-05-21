fa09e98d543372db8adb0658d800665144d28134
Diogo Silva <diogo@diogo-silva.pt>
2019-05-21 22:54:14 +0100

    Games Main Screen

--- src/components/games/index.js
+++ src/components/games/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 import { connect } from 'react-redux';
 import { getGames } from '../../store/actions/gamesActions';
@@ -11,19 +11,86 @@ class GamesComponent extends Component {
         this.props.dispatch(getGames());
     }
 
+    showGames = (list) => (
+        list.games ? 
+            list.games.map((item, i) => (
+                <TouchableOpacity
+                    key={i}
+                    onPress={() => this.props.navigation.navigate('Article', {
+                        ...item
+                    })}
+                >
+                    <View style={styles.gameContainer}>
+
+                        <View style={styles.gamebox}>
+                            <Image
+                                source={{uri: `${item.localData.logo}`}}
+                                style={{height: 80, width: 80}}
+                                resizeMode='contain'
+                            />
+                            <Text style={styles.teamRecord}>{item.localData.wins} - {item.localData.loss}</Text>
+                        </View>
+
+                        <View style={styles.gamebox}>
+                            <Text style={styles.gameTime}>{item.time}</Text>
+                            <Text>{Moment(item.date).format('d MMMM')}</Text>
+                        </View>
+
+                        <View style={styles.gamebox}>
+                        <Image
+                                source={{uri: `${item.awayData.logo}`}}
+                                style={{height: 80, width: 80}}
+                                resizeMode='contain'
+                            />
+                            <Text style={styles.teamRecord}>{item.awayData.wins} - {item.awayData.loss}</Text>
+                        </View>
+
+                    </View>
+
+                </TouchableOpacity>
+            ))
+        :
+            <ActivityIndicator/>
+    ) 
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>GamesComponent</Text>
-            </View>
+            <ScrollView style={{backgroundColor: '#F0F0F0'}}>
+                <View style={{flex: 1, flexDirection:'column', flexWrap:'nowrap'}}>
+                    { this.showGames(this.props.Games) }
+                </View>
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    gameContainer: {
         flex: 1,
-        backgroundColor: '#FFFFFF',
+        flexDirection:'row',
+        marginBottom: 10,
+        backgroundColor: '#fff',
+        shadowColor: '#dddddd',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 1,
+        borderRadius: 2
+    },
+    gamebox: {
+        width: '33.33%',
+        height: 100,
+        backgroundColor:'#fff',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    teamRecord:{
+        fontFamily:'Roboto-Light',
+        fontSize: 12
+    },
+    gameTime:{
+        fontFamily: 'Roboto-Bold',
+        fontSize: 15
     }
 });
 
